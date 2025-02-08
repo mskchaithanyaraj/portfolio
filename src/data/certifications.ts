582d05967ec953431b8e3f7340b02846a21834f8
Sri Krishna Chaithanya Raj Masimukku <mskchaithanyaraj@gmail.com>
2025-02-09 00:06:21 +0530

    updated image paths

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -17,7 +17,7 @@ export const certifications: Certification[] = [
   {
     id: uuidv4(),
     name: "CodeIT'23",
-    imageUrl: "/public/assets/certifications/codeit.png",
+    imageUrl: "/assets/certifications/codeit.png",
     category: "Intercollege Competitions",
     issuer: "SRGEC",
     date: "September 2023",
@@ -25,7 +25,7 @@ export const certifications: Certification[] = [
   {
     id: uuidv4(),
     name: "Introduction to IOT",
-    imageUrl: "/public/assets/certifications/iot_nptel.png",
+    imageUrl: "/assets/certifications/iot_nptel.png",
     category: "NPTEL",
     issuer: "NPTEL",
     date: "November 2024",
@@ -33,7 +33,7 @@ export const certifications: Certification[] = [
   {
     id: uuidv4(),
     name: "Programming Foundations with Python",
-    imageUrl: "/public/assets/certifications/python.png",
+    imageUrl: "/assets/certifications/python.png",
     category: "Course Completions",
     issuer: "NxtWave",
     date: "June 14, 2023",
@@ -41,7 +41,7 @@ export const certifications: Certification[] = [
   {
     id: uuidv4(),
     name: "Build Your Own Responsive Website",
-    imageUrl: "/public/assets/certifications/Responsive.png",
+    imageUrl: "/assets/certifications/Responsive.png",
     category: "Course Completions",
     issuer: "NxtWave",
     date: "April 05, 2023",
@@ -49,7 +49,7 @@ export const certifications: Certification[] = [
   {
     id: uuidv4(),
     name: "Build Your Own Static Website",
-    imageUrl: "/public/assets/certifications/Static.png",
+    imageUrl: "/assets/certifications/Static.png",
     category: "Course Completions",
     issuer: "NxtWave",
     date: "November 03, 2022",
@@ -57,7 +57,7 @@ export const certifications: Certification[] = [
   {
     id: uuidv4(),
     name: "How to become Data Scientist",
-    imageUrl: "/public/assets/certifications/podcasts/data_scientist.jpg",
+    imageUrl: "/assets/certifications/podcasts/data_scientist.jpg",
     category: "Podcasts",
     issuer: "NxtWave",
   },
